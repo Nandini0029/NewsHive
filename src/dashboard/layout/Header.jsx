9049e8949134d5cad3a9d2968b2444d29ace6046
Anuj Mondal <hydranuj@gmail.com>
2025-03-08 21:55:14 +0530

    header

--- src/dashboard/layout/Header.jsx
+++ src/dashboard/layout/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import profile from '../../assets/profile.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className='pl-4 fixed w-[calc(100vw-250px)] top-4 z-50'>
+      <div className='w-full rounded h-[70px] flex justify-between items-center p-4 bg-white'>
+        <input 
+          type='text' 
+          placeholder='Search...' 
+          className='px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10 w-1/3'
+        />
 
-export default Header
+        <div className='mr-4'>
+          <div className='flex gap-x-2'>
+            <div className='flex flex-col justify-center items-end'>
+              <span className='font-semibold'>Nandini Kapil</span>
+              <span className='text-sm text-gray-500'>Admin</span>
+            </div>
+            <img 
+              className='w-10 h-10 rounded-full border border-gray-300' 
+              src={profile} 
+              alt='Profile' 
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
+// Let me know if you want me to tweak anything or add features! 🚀
--- src/dashboard/layout/MainLayout.jsx
+++ src/dashboard/layout/MainLayout.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import SideBar from './SideBar';
-import Header from './Header';
-import { Outlet } from 'react-router-dom';
+import React from 'react'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
     return (
         <div className='min-w-screen min-h-screen bg-slate-100'>
-            <SideBar />
-        </div>
-    );
-};
+            <Sidebar />
+            <div className='ml-[250px] w-[calc(100vw-268px)] min-h-[100vh]'>
+                <Header />
+                <div className='p-4'>
+                    <div className='pt-[85px]'>
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
 
-export default MainLayout;
+        </div>
+    )
+}
+
+export default MainLayout
